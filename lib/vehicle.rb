c1d118f46ed48386025406988f841f58b4fe02c3
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-21 13:00:41 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,5 +2,7 @@ class Vehicle #parent class
 
   attr_accessor :wheel_size, :wheel_number #initialize
 
-
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel size
+    @wheel_number = wheel_number
 end
