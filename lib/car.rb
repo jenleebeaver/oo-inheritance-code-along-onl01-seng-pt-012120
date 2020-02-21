0c5c36010d9ca2176e11e5efeefa8370eb0c7751
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-21 13:10:10 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,6 +1,9 @@
 #steps to define the subclass
 
-require_relative "./vehicle.rb" #allows us to access the parent class
+require_relative "./vehicle.rb" #allows us to access 
+                                #the parent class
 
-class Car < Vehicle #here we are defining the class. The < inherits Car from Vehicle 
+class Car < Vehicle #here we are defining the class. 
+                    #The < inherits Car from Vehicle 
+                    #and inherits all of its methods. 
 end
